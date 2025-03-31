859d882424d411eb732dd9cba84b953c9cd2d94b
Odin <Odin@Valkyrie.com>
2025-03-31 00:03:37 -0600

    Este es un commit escrito desde VS Code, porque no me gusto Vim

--- basis_2/all_function.py
+++ basis_2/all_function.py
@@ -1,5 +1,11 @@
+# Podriamos decir que es el operador AND pero de las listas.
+# Ya que solo devuelve True si todos los elementos son True.
+
 # `all()` devuelve `True` si todos los elementos del iterable son `True`.
 # Si alguno de los elementos es `False`, devuelve `False`. Si el iterable está vacío, retorna `True`.
+
+# Es parecida a `any()`, pero en lugar de verificar si al menos un elemento es `True`,
+# verifica si todos los elementos son `True`.
 
 # Ejemplo 1: Verificar si todos los números son positivos.
 numeros = [1, 2, 3, 4]
--- basis_2/map_function_lambda.py
+++ basis_2/map_function_lambda.py
@@ -21,36 +21,3 @@ print(list(plurales))
 # Salida: ['myS', 'worldS', 'appleS', 'electroencefalografiaS', 'pearS']
 # Notese que la función lambda se define en una sola línea y no tiene nombre.
 
-print("")
-print("Ejemplo 3: map() con filter()")
-def longer_than_10(word):
-    """ La función longer_than_5 toma una palabra y devuelve True si su longitud es mayor a 5. """
-    return len(word) > 10
-
-filtrado = filter(longer_than_10, strings)
-# La función filter() toma una función y un iterable y devuelve un nuevo iterable con los elementos 
-# que cumplen la condición. En este caso, las palabras que tienen mas de 10 letras.
-print(list(filtrado))
-# Salida: ['electroencefalografia']
-
-print("")
-print("Ejemplo 4: map() con filter()")
-# Ahora filtremos las palabras que tienen menos de 4 letras
-def shorter_than_5(word):
-    """ La función shorter_than_5 toma una palabra y devuelve True si su longitud es menor a 5. """
-    return len(word) < 5
-
-filtra2 = filter(shorter_than_5, strings)
-print(list(filtra2))
-# Salida: ['my', 'pear']
-# Notese que no se incluyen las palabras que tienen 5 letras, ya que el parametro
-# de la funcion filter() es menor a 5. No menor o igual a 5.
-# Por eso solo tenemos las palabras de 4 letras o menos.
-
-print("")
-print("Ejemplo 5: map() con filter() y lambda")
-# Ahora hagamos lo mismo pero con lambda
-filtra3 = filter(lambda x: len(x) < 5, strings)
-print(list(filtra3))
-# Salida: ['my', 'pear']
-# La función lambda toma cada palabra y devuelve True si su longitud es menor a 5.
